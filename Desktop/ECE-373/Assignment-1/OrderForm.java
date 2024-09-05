b66a48c65d1829f82632de9485666a3f0d43d0ba
jimmyPayan <jimmypayan@arizona.edu>
2024-09-05 15:00:54 -0700

    Change two?

---
+++ Desktop/ECE-373/Assignment-1/OrderForm.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class OrderForm here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class OrderForm
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class OrderForm
+     */
+    public OrderForm()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+        // put your code here
+        System.out.print("Hello World!");
+    }
+}
